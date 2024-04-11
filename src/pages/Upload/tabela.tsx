bc561d49a2e82778f357f94b8bc8ead4172b9507
Mariana Tamay <maritamay4@gmail.com>
2024-04-11 19:23:36 -0300

    ✨API-46: Integração da tabela de histórico de arquivos enviados para upload

--- src/pages/Upload/tabela.tsx
+++ src/pages/Upload/tabela.tsx
@@ -1,21 +1,44 @@
+import axios from "axios";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
+import { useEffect, useState } from "react";
+import url from "../../services/config";
+import { IFile } from "../../interfaces/file";
 
 export default function TabelaUpload(){
-    const products = [
-        { data: 'DD/MM/AAAA', name: 'Arquivo.csv', size: '50 MB', status: "Pré-processamento" },
-        { data: 'DD/MM/AAAA', name: 'Arquivo.csv 2', size: '40 MB', status: "Pré-processamento" },
-        { data: 'DD/MM/AAAA', name: 'Arquivo.csv 3', size: '100 MB', status: "Processando" },
-        { data: 'DD/MM/AAAA', name: 'Arquivo.csv 4', size: '80 MB', status: "Concluído" },
-        // Adicione mais produtos conforme necessário
-    ];
+    const [file, setFile] = useState<IFile[]>([]); 
+
+    useEffect(() => {
+        axios.get<IFile[]>(`${url.baseURL}/base-importer/allBases`)
+            .then(response => {
+                const formattedData = response.data.map(item => ({
+                    ...item,
+                    createdAt: formatDateString(item.createdAt),
+                    status: item.status === 'success' ? 'Concluído!' : 'Erro ao enviar arquivo'
+                }));
+                setFile(formattedData);
+            })
+            .catch(error => {
+                console.error('Erro ao obter dados:', error);
+            });
+    }, []);
+
+    const formatDateString = (dateString: string) => {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+    };
+
     return(
         <>
             <div className="cont-table">
-                <DataTable value={products} stripedRows tableStyle={{ minWidth: '50rem' }} size={"normal"}>
-                    <Column field="data" header="Data da importação"></Column>
-                    <Column field="name" header="Nome do arquivo"></Column>
-                    <Column field="size" header="Tamanho"></Column>
+                <DataTable value={file} stripedRows tableStyle={{ minWidth: '50rem' }} size={"normal"}>
+                    <Column field="createdAt" header="Data da importação"></Column>
+                    <Column field="fileName" header="Nome do arquivo"></Column>
                     <Column field="status" header="Status"></Column>
                 </DataTable>
             </div>
