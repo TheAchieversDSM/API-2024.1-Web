bd78bdc3bf455ac5f734a70edcb89e8e53bf17e0
Maria Clara Alves de Faria <mclaralvs@gmail.com>
2024-06-11 21:11:10 -0300

    API-121 🧪: Adicionando caso de falha.

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,14 @@
 describe('Login', () => {
-  it('passes', () => {
+  it('should fail bc used wrong user', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[name="email"]').type("teste@email.com");
+    cy.get('[name="email"]').type("teste@email.com")
     cy.get('[name="pwd"]').type("senha")
+    cy.get('[name="button"]').click()
+  })
+  it('should succeed bc used existent user', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[name="email"]').type("maria@email.com")
+    cy.get('[name="pwd"]').type("segredo")
     cy.get('[name="button"]').click()
   })
 })
